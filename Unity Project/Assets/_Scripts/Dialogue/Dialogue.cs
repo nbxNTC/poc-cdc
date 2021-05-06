7fd980b3672bfa8c60f4201a3dd7b52a0d1a1114
Mario Sergio <marionbxcz@gmail.com>
2021-05-06 01:33:44 -0300

    feat: new dialogue system

---
+++ Unity Project/Assets/_Scripts/Dialogue/Dialogue.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialogue : MonoBehaviour
+{
+    [Header("Dialog Management System")]
+    public GameObject dialogueObject;
+    public Dialogue dialogue;
+    public DialogueSystem dialogueSystem;
+
+    [Header("Dialogue Info")]
+    public int id;
+    public int dialogueIndex = 0;
+    public List<string> dialogues;
+
+    [Header("Dialogue Enable/Disable Object")]
+    public bool willEnableObject = false;
+    public GameObject objectToEnable;
+    public bool willDisableObject = false;
+    public GameObject objectToDisable;
+
+    [Header("Dialogue Next Position")]
+    public bool hasNextPosition = false;
+    public float nextXPosition;
+    public float nextYPosition;
+
+    [Header("Dialogue Next Camera Positions")]
+    public bool hasNextCameraPositions = false;
+    public Vector2 nextMaxPosition;
+    public Vector2 nextMinPosition;
+
+    private void OnTriggerEnter2D (Collider2D other) {
+        if (other.gameObject.tag == "Player") {
+            Player player = other.GetComponent<Player>();
+
+            if (willEnableObject) objectToDisable.SetActive(true);
+            if (willDisableObject) objectToDisable.SetActive(false);
+
+            if (this.id == -1) {
+                dialogueSystem.StartDialog(dialogue, player);
+                dialogueObject.SetActive(false);
+            }
+
+            if (this.id == player.currentDialogId) {
+                player.currentDialogId += 1;
+                dialogueSystem.StartDialog(dialogue, player);
+                dialogueObject.SetActive(false);
+            }
+        }
+    }
+}
---
+++ Unity Project/Assets/_Scripts/Dialogue/DialogueSystem.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueSystem : MonoBehaviour
+{   
+    [Header("Dialogue UI")]
+    public GameObject dialogueUI;
+    public Text dialogueText;
+    public GameObject loadingUI;
+
+    [Header("Dialogue Buttons")]
+    public GameObject next;
+
+    [Header("Dialogue Control")]
+    public Dialogue current;
+    private Player player;
+    public CameraMovement cameraMovement;
+
+    void Update() {
+        if (current != null) {
+            dialogueText.text = current.dialogues[current.dialogueIndex];
+        } else {
+            dialogueText.text = "";
+        }
+    }
+
+    public void StartDialog(Dialogue dialogue, Player player) {    
+        this.player = player;
+        this.player.canMove = false;
+
+        dialogueUI.SetActive(true);
+        current = dialogue;
+    }
+
+    public void ExitDialog() {
+        dialogueUI.SetActive(false);
+        player.canMove = true;
+    }
+
+    public void onNextButton() {
+        if (current.dialogueIndex < current.dialogues.Count - 1) {
+            current.dialogueIndex += 1;
+        } else {
+            if (current.hasNextPosition) {
+                loadingUI.SetActive(true);
+
+                if (current.hasNextCameraPositions) {
+                    cameraMovement.maxPosition = current.nextMaxPosition;
+                    cameraMovement.minPosition = current.nextMinPosition;
+                }
+
+                player.transform.position = new Vector2(current.nextXPosition, current.nextYPosition);
+                StartCoroutine(DisableLoading());
+            }
+
+            ExitDialog();
+        }
+    }
+
+    IEnumerator DisableLoading () {
+        yield return new WaitForSeconds(4);
+
+        loadingUI.SetActive(false);
+    }
+}
+

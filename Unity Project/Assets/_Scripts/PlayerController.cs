f40e29793d3c07b7ef96e4eeb1792856793e9486
Mario Sergio <marionbxcz@gmail.com>
2021-12-14 10:02:26 -0300

    feat: implements d-pad

--- Unity Project/Assets/_Scripts/PlayerController.cs
+++ Unity Project/Assets/_Scripts/PlayerController.cs
@@ -9,6 +9,13 @@ public class PlayerController : MonoBehaviour
     [Header("Player")]
     public Player player;
 
+    [Header("D-Pad")]
+    public GameObject DPadUI;
+    public DPadButton topButton;
+    public DPadButton downButton;
+    public DPadButton leftButton;
+    public DPadButton rightButton;
+
     [Header("Animator")]
     private Animator _animator;
 
@@ -27,32 +34,47 @@ public class PlayerController : MonoBehaviour
     private void Update() {
         Movement();
 
-        if (Input.GetMouseButtonDown(0)) {
-            player.SavePlayer();
+        // if (Input.GetMouseButtonDown(0)) {
+        //     player.SavePlayer();
 
-            // int index = 1;
-            // List<GameObject> list = new List<GameObject>();
-            // GameObject test;
-            // do {
-            //     test = GameObject.Find("Dialogue_0" + index);
-            //     Debug.Log("Dialogue_0" + index);
-            //     if (test != null) {
-            //         list.Add(test);
-            //     }
-            //     index += 1;
-            // } while (test != null);
+        //     // int index = 1;
+        //     // List<GameObject> list = new List<GameObject>();
+        //     // GameObject test;
+        //     // do {
+        //     //     test = GameObject.Find("Dialogue_0" + index);
+        //     //     Debug.Log("Dialogue_0" + index);
+        //     //     if (test != null) {
+        //     //         list.Add(test);
+        //     //     }
+        //     //     index += 1;
+        //     // } while (test != null);
 
-            // Debug.Log(list.Count);
-        }
+        //     // Debug.Log(list.Count);
+        // }
 
-        if (Input.GetMouseButtonDown(1)) {
-            player.LoadPlayer();
-        }
+        // if (Input.GetMouseButtonDown(1)) {
+        //     player.LoadPlayer();
+        // }
     }
 
     private void Movement() {
-        _movementInput.x = Input.GetAxisRaw("Horizontal");
-        _movementInput.y = Input.GetAxisRaw("Vertical");
+        if (player.canMove) DPadUI.SetActive(true);
+        if (!player.canMove) {
+            DPadUI.SetActive(false);
+            topButton.buttonPressed = false;
+            downButton.buttonPressed = false;
+            leftButton.buttonPressed = false;
+            rightButton.buttonPressed = false;
+        }
+
+        if (topButton.buttonPressed) _movementInput.y = 1;
+        if (downButton.buttonPressed) _movementInput.y = -1;
+        if (leftButton.buttonPressed) _movementInput.x = -1;
+        if (rightButton.buttonPressed) _movementInput.x = 1;
+
+        if (!topButton.buttonPressed && !downButton.buttonPressed && !leftButton.buttonPressed && !rightButton.buttonPressed) {
+            _movementInput = Vector2.zero;
+        }
 
         if (_movementInput != Vector2.zero && player.canMove) {
             _myRigidbody.MovePosition(transform.position += (Vector3)_movementInput * player.entity.speed * Time.deltaTime);
